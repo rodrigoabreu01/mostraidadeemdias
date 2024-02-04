54e939f36b354de2d3d65aeb238a8ebc36bc32d3
rodrigoabreu01 <digobbca@gmail.com>
2024-02-04 20:21:05 -0300

    nova alteracao

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+let data = new Date();
+let diaAtual = data.getDay();
+let mesAtual = data.getMonth();
+let anoAtual = data.getFullYear();
+
+
+
 
 
 formulario.onsubmit = () => {
-    event.preventDefault();
+    event.preventDefault()
+    
+    let anos = anoAtual - valorAno.value;
+    let mes = mesAtual - valorMes.value;
+    let dia = diaAtual - valorDia.value;
+
+    if(valorDia.value === "" || valorMes.value === "" || valorAno.value === ""){
+        alert('Opa!! Falta preencher!');
+    }else{
+        mostraAno.innerHTML = anos  ;
+        mostraMes.innerHTML =  anos * 12;
+        mostraDias.innerHTML =  anos * 365;
+    }
 
 
-     mostraAno.innerTEXT = valorAno.value;
-     mostraMes.innerTEXT = valorMes.value;
-     mostraDias.innerTEXT = valorDia.value;
+
+    
 }
+
+
